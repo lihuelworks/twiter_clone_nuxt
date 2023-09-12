24e72edfa2600e5b7ff07d069a6f4073aba116c7
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-09-11 21:56:07 -0300

    letsago

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="{ 'dark': darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:gap-5 lg:px-8">
+          <!-- Left sidebar-->
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- Main content -->
+          <!-- Right sidebar -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const darkMode = ref(true)
+</script>
+
+<style scoped></style>
---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,29 @@
+<template>
+    <NuxtLink to="#"
+        class="flex items-center p-3 text-black w-min rounded-full hover:bg-gray-200 dark:hover:bg-dim-200 dark:text-white">
+
+        <div class="w-6 h-6 text-dark" :class="defaultTransition">
+            <slot name="icon"></slot>
+        </div>
+        <div class="hidden ml-4 text-xl xl:block" :class="textClasses">
+            <slot name="name"></slot>
+        </div>
+
+    </NuxtLink>
+</template>
+
+<script setup>
+const { defaultTransition } = useTailwindConfig()
+
+const props = defineProps({
+    active: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const textClasses = computed(() => props.active ? 'font-semibold' : 'font-normal')
+
+</script>
+
+<style lang="scss" scoped></style>
